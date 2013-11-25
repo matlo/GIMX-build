8cab7f3294fd571a1df610d9edad067537dfac45
Matlo <mat.lau@laposte.net>
2013-11-25 22:05:37 +0100

    Migrate from svn to git.

--- win/build.sh
+++ win/build.sh
@@ -12,21 +12,11 @@ then
 else
   CPU=1
 fi
-PREFIX=/mingw
-SVN_VERSION=1.6.17
-
-#install svn
-if ! test -f /usr/local/bin/svn.exe
-then
-  wget http://diyps3controller.googlecode.com/files/svn-win32-$SVN_VERSION.zip
-  unzip -u -d /usr/local/ svn-win32-$SVN_VERSION.zip
-  rm -rf svn-win32-$SVN_VERSION svn-win32-$SVN_VERSION.zip
-fi
 
 #checkout & compile GIMX
 if ! test -d GIMX
 then
-  svn checkout http://diyps3controller.googlecode.com/svn/trunk/GIMX
+  git clone -b master --single-branch --depth 1 https://github.com/matlo/GIMX.git
 fi
 
 if [ -n "$VERSION" ]
