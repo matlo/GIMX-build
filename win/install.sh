bdfdefeec451a2d86687ef3e17b9e25b00ec76db
Matlo <mat.lau@laposte.net>
2014-06-07 19:30:19 +0200

    Download files in a temp folder.

--- win/install.sh
+++ win/install.sh
@@ -11,16 +11,20 @@ fi
 
 pacman --noconfirm -S mingw-w64-$ARCH-gcc mingw-w64-$ARCH-SDL2 mingw-w64-$ARCH-libxml2 mingw-w64-$ARCH-curl
 
+mkdir gimx-install
+cd gimx-install
+
 wget https://github.com/matlo/GIMX-tools/releases/download/1.0/mingw-w64-$ARCH-libusb-1.0.18-1-any.pkg.tar.xz
 wget https://github.com/matlo/GIMX-tools/releases/download/1.0/mingw-w64-$ARCH-pdcurses-3.4.0-1-any.pkg.tar.xz
 wget https://github.com/matlo/GIMX-tools/releases/download/1.0/mingw-w64-$ARCH-wxMSW-2.8.12-1-any.pkg.tar.xz
 
 pacman --noconfirm -U *.pkg.tar.xz
 
-rm *.pkg.tar.xz
-
 wget https://raw.githubusercontent.com/matlo/GIMX-tools/master/MINGW-packages/patches/hidsdi.h.patch
 wget https://raw.githubusercontent.com/matlo/GIMX-tools/master/MINGW-packages/patches/hidclass.h.patch
 
 patch -d /$MSYSTEM/$ARCH-w64-$MSYSTEM/include -p0 < hidsdi.h.patch
 patch -d /$MSYSTEM/$ARCH-w64-$MSYSTEM/include/ddk -p0 < hidclass.h.patch
+
+cd ..
+rm -rf gimx-install
