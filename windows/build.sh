8025425018f65fd357fa2dad92920c2bfbfb69f2
Matlo <mat.lau@laposte.net>
2014-11-06 08:27:49 +0100

    Cleaning.

--- windows/build.sh
+++ windows/build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-#
-# Warning: changing the version will clean the sources and remove the setup directory!!!!
-#
-VERSION=$1
-OLDVERSION=$(grep INFO_VERSION GIMX/info.h 2> /dev/null | sed "s/#define[ ]*INFO_VERSION[ ]*//" | sed "s/\"//g")
+VERSION=`cat version`
+DATE=`date -R`
 BRANCH="master"
+
+echo "Version number? (default: "$VERSION")"
+
+read NEW_VERSION
+
+if [ -z $NEW_VERSION ]
+then
+  NEW_VERSION=$VERSION
+fi
+
+if [ -z $NEW_VERSION ]
+then
+  echo No version specified!
+  exit
+fi
 
 if test -f cpu
 then
@@ -15,65 +27,50 @@ else
 fi
 
 #checkout & compile GIMX
-if ! test -d GIMX
+echo Clean previous build.
+rm -rf GIMX
+git clone -b $BRANCH --single-branch --depth 1 https://github.com/matlo/GIMX.git
+
+MAJOR=$(echo $NEW_VERSION | awk -F"." '{print $1}')
+MINOR=$(echo $NEW_VERSION | awk -F"." '{print $2}')
+echo Major release number: $MAJOR
+echo Minor release number: $MINOR
+if [ -z $MAJOR ] || [ -z $MINOR ]
 then
-  git clone -b $BRANCH --single-branch --depth 1 https://github.com/matlo/GIMX.git
+  echo Invalid release number!
+  exit
 fi
 
-if [ -n "$VERSION" ]
-then
-  MAJOR=$(echo $VERSION | awk -F"." '{print $1}')
-  MINOR=$(echo $VERSION | awk -F"." '{print $2}')
-  echo Major release number: $MAJOR
-  echo Minor release number: $MINOR
-  if [ -z $MAJOR ] || [ -z $MINOR ]
-  then
-    echo Invalid release number!
-    exit
-  fi
-  
-  sed -i "s/FILEVERSION[ ]*[0-9]*,[0-9]*,[0-9]*,[0-9]*/FILEVERSION   $MAJOR,$MINOR,0,0/" GIMX/*/*.rc
-  sed -i "s/PRODUCTVERSION[ ]*[0-9]*,[0-9]*,[0-9]*,[0-9]*/PRODUCTVERSION  $MAJOR,$MINOR,0,0/" GIMX/*/*.rc
-  sed -i "s/[ ]*VALUE[ ]*\"FileVersion\",[ ]*\"[0-9]*.[0-9]*\"/    VALUE \"FileVersion\", \"$MAJOR.$MINOR\"/" GIMX/*/*.rc
-  sed -i "s/[ ]*VALUE[ ]*\"ProductVersion\",[ ]*\"[0-9]*.[0-9]*\"/    VALUE \"ProductVersion\", \"$MAJOR.$MINOR\"/" GIMX/*/*.rc
-  
-  sed -i "s/#define[ ]*INFO_VERSION[ ]*\"[0-9]*.[0-9]*\"/#define INFO_VERSION \"$MAJOR.$MINOR\"/" GIMX/info.h
-  sed -i "s/#define[ ]*INFO_YEAR[ ]*\"2010-[0-9]*\"/#define INFO_YEAR \"2010-$(DATE '+%Y')\"/" GIMX/info.h
-  
-  sed -i "s/#define[ ]*MyAppVersion[ ]*\"[0-9]*.[0-9]*\"/#define MyAppVersion \"$MAJOR.$MINOR\"/" inno.iss
-fi
+sed -i "s/FILEVERSION[ ]*[0-9]*,[0-9]*,[0-9]*,[0-9]*/FILEVERSION   $MAJOR,$MINOR,0,0/" GIMX/*/*.rc
+sed -i "s/PRODUCTVERSION[ ]*[0-9]*,[0-9]*,[0-9]*,[0-9]*/PRODUCTVERSION  $MAJOR,$MINOR,0,0/" GIMX/*/*.rc
+sed -i "s/[ ]*VALUE[ ]*\"FileVersion\",[ ]*\"[0-9]*.[0-9]*\"/    VALUE \"FileVersion\", \"$MAJOR.$MINOR\"/" GIMX/*/*.rc
+sed -i "s/[ ]*VALUE[ ]*\"ProductVersion\",[ ]*\"[0-9]*.[0-9]*\"/    VALUE \"ProductVersion\", \"$MAJOR.$MINOR\"/" GIMX/*/*.rc
+
+sed -i "s/#define[ ]*INFO_VERSION[ ]*\"[0-9]*.[0-9]*\"/#define INFO_VERSION \"$MAJOR.$MINOR\"/" GIMX/info.h
+sed -i "s/#define[ ]*INFO_YEAR[ ]*\"2010-[0-9]*\"/#define INFO_YEAR \"2010-$(DATE '+%Y')\"/" GIMX/info.h
+
+sed -i "s/#define[ ]*MyAppVersion[ ]*\"[0-9]*.[0-9]*\"/#define MyAppVersion \"$MAJOR.$MINOR\"/" inno.iss
 
 cd GIMX
-
-if [ -n "$VERSION" ] && [ "$VERSION" != "$OLDVERSION" ]
-then
-  echo Version changed: clean before build.
-  make clean
-  rm -rf setup
-fi
-
 make -j $CPU install
-
 cd ..
 
-if [ -n "$VERSION" ]
+if [ "$MSYSTEM" == "MINGW32" ]
 then
-  if [ "$MSYSTEM" == "MINGW32" ]
-  then
-    sed -i "s/[; ]*ArchitecturesInstallIn64BitMode/; ArchitecturesInstallIn64BitMode/" -i inno.iss
-  else
-    sed -i "s/[; ]*ArchitecturesInstallIn64BitMode/ArchitecturesInstallIn64BitMode/" -i inno.iss
-  fi
-  if test -f /c/Program\ Files\ \(x86\)/Inno\ Setup\ 5/ISCC.exe
-  then
-    /c/Program\ Files\ \(x86\)/Inno\ Setup\ 5/ISCC.exe inno.iss
-  else
-    /c/Program\ Files/Inno\ Setup\ 5/ISCC.exe inno.iss
-  fi
-  if [ "$MSYSTEM" == "MINGW32" ]
-  then
-    mv gimx-$VERSION.exe gimx-$VERSION-i386.exe
-  else
-    mv gimx-$VERSION.exe gimx-$VERSION-x86_64.exe
-  fi
+  sed -i "s/[; ]*ArchitecturesInstallIn64BitMode/; ArchitecturesInstallIn64BitMode/" -i inno.iss
+else
+  sed -i "s/[; ]*ArchitecturesInstallIn64BitMode/ArchitecturesInstallIn64BitMode/" -i inno.iss
+fi
+if test -f /c/Program\ Files\ \(x86\)/Inno\ Setup\ 5/ISCC.exe
+then
+  /c/Program\ Files\ \(x86\)/Inno\ Setup\ 5/ISCC.exe inno.iss
+else
+  /c/Program\ Files/Inno\ Setup\ 5/ISCC.exe inno.iss
+fi
+
+if [ "$MSYSTEM" == "MINGW32" ]
+then
+  mv gimx-$VERSION.exe gimx-$VERSION-i386.exe
+else
+  mv gimx-$VERSION.exe gimx-$VERSION-x86_64.exe
 fi
