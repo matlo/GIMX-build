a991a4150426a518ba49517be956d33d3cffc7c7
Mathieu Laurendeau <mat.lau@laposte.net>
2016-07-08 17:34:39 +0200

    Use wxWidgets from the repository.

--- windows/install.sh
+++ windows/install.sh
@@ -17,16 +17,14 @@ pacman --needed --noconfirm -S \
   mingw-w64-$ARCH-gcc \
   mingw-w64-$ARCH-libxml2 \
   mingw-w64-$ARCH-curl \
-  mingw-w64-$ARCH-SDL2
-#  mingw-w64-$ARCH-wxWidgets
+  mingw-w64-$ARCH-SDL2 \
+  mingw-w64-$ARCH-wxWidgets
 
 mkdir gimx-install
 cd gimx-install
 
 wget https://github.com/matlo/GIMX-tools/releases/download/${PACKAGE_RELEASE}/mingw-w64-$ARCH-pdcurses-3.4.0-1-any.pkg.tar.xz
 wget https://github.com/matlo/GIMX-tools/releases/download/${PACKAGE_RELEASE}/mingw-w64-$ARCH-libusb-usbdk-0.0.0-1-any.pkg.tar.xz
-
-wget https://github.com/matlo/GIMX-tools/releases/download/4.0/mingw-w64-$ARCH-wxWidgets-3.0.2-12-any.pkg.tar.xz
 
 pacman --needed --noconfirm -U *.pkg.tar.xz
 
