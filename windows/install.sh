ca25c568094a6b5d7b5885878d09fd9ec83d2860
matlo <mat.lau@laposte.net>
2017-09-27 23:15:37 +0200

    Update for GIMX 7.0.

--- windows/install.sh
+++ windows/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGE_RELEASE="6.0"
+PACKAGE_RELEASE="7.0"
 
 if test "$MSYSTEM" = "MINGW32"
 then
@@ -9,7 +9,7 @@ else
   ARCH=x86_64
 fi
 
-pacman --needed --noconfirm -S wget man git make diffutils patch p7zip unzip
+pacman --needed --noconfirm -S wget man git make diffutils patch p7zip unzip automake libtool
 
 pacman --force --needed --noconfirm -S mingw-w64-$ARCH-toolchain
 
@@ -19,14 +19,14 @@ pacman --needed --noconfirm -S \
   mingw-w64-$ARCH-gcc \
   mingw-w64-$ARCH-libxml2 \
   mingw-w64-$ARCH-curl \
-  mingw-w64-$ARCH-wxWidgets
+  mingw-w64-$ARCH-wxWidgets \
+  mingw-w64-$ARCH-libusb-usbdk
 
 mkdir gimx-install
 cd gimx-install
 
 wget https://github.com/matlo/GIMX-tools/releases/download/${PACKAGE_RELEASE}/mingw-w64-$ARCH-pdcurses-3.4.0-1-any.pkg.tar.xz
-wget https://github.com/matlo/GIMX-tools/releases/download/${PACKAGE_RELEASE}/mingw-w64-$ARCH-libusb-usbdk-0.0.0-1-any.pkg.tar.xz
-wget https://github.com/matlo/GIMX-tools/releases/download/${PACKAGE_RELEASE}/mingw-w64-$ARCH-SDL2-2.0.4-1-any.pkg.tar.xz
+wget https://github.com/matlo/GIMX-tools/releases/download/${PACKAGE_RELEASE}/mingw-w64-$ARCH-SDL2-2.0.6-1-any.pkg.tar.xz
 wget https://github.com/matlo/GIMX-tools/releases/download/${PACKAGE_RELEASE}/mingw-w64-$ARCH-avrdude-6.3-1-any.pkg.tar.xz
 
 pacman --needed --noconfirm -U *.pkg.tar.xz
